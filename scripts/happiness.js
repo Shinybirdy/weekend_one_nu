0206377443d21e60f3a4c951b8cb5243f8ce8bb7
Tracey Zavadil <shinybirdy@yahoo.com>
2016-05-28 15:51:14 -0500

    initial commit

---
+++ scripts/happiness.js
@@ -0,0 +1,19 @@
+console.log("hey");
+function addEmp() {
+    var firstName =document.getElementById("firstName").value;
+    var lastName =document.getElementById("lastName").value;
+    var empId =document.getElementById('empId').value;
+    var jobTitle =document.getElementById('jobTitle').value;
+    var annSalary =document.getElementById('annSalary').value;
+console.log(firstName + " " + lastName + " " + empId + " " + jobTitle + " " + annSalary + ": ");
+
+}
+
+
+//take in Employee field info
+//make an employee object! :)
+//this is just like the table objects from Dev's lecture! :)
+var createNewEmp = function(){
+
+    console.log(firstName);
+};
